31caf9faf86d361e2bc4ed5fe7ce8cb18eaf071f
1508saif <safiullahtaiyab@gmail.com>
2026-07-30 23:29:02 +0530

    Basic JavaScript

---
+++ arrScript.js
@@ -0,0 +1,10 @@
+//  Js Series beginer to industry level
+
+const myArr = [0,2,23,42,20,42];
+const meHeroes =["shaktimaan" , "devnaag"];
+const myArr2 = new Array(2,3,43,32);
+console.log(myArr2[2]);
+
+myArr.push(7);
+myArr.pop();
+console.log(myArr);
